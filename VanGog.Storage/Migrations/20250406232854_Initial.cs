47467174b2d8f6b9d40a2118177ad0d1f28daeb1
Ulyana Novoselova <uliana.a.novoselova@gmail.com>
2025-04-12 00:14:57 +0300

    бд

---
+++ VanGog.Storage/Migrations/20250406232854_Initial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VanGog.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Time = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    Participants = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false),
+                    ImagePath = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
--- VanGog.Storage/VanGogDbContext.cs
+++ VanGog.Storage/VanGogDbContext.cs
@@ -4,8 +4,19 @@ using VanGog.Storage.Core.Entities;
 
 namespace VanGog.Storage
 {
-    public class VanGogDbContext(DbContextOptions<VanGogDbContext> options) : DbContext(options)
+    public class VanGogDbContext : DbContext
     {
         public DbSet<Event> Events { get; set; }
+
+        public VanGogDbContext() { }
+        public VanGogDbContext(DbContextOptions<VanGogDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=vangog-db;User ID=postgres;Password=902692A2006UN951967;");
+            }
+        }
     }
 }
